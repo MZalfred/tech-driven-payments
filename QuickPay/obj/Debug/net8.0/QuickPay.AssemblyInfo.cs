d2fe7d40ef0297c9be4dacc7d0824d80966439ec
MZalfred <cutesy-todo0x@icloud.com>
2024-02-27 06:13:23 +0000

    main

--- QuickPay/obj/Debug/net8.0/QuickPay.AssemblyInfo.cs
+++ QuickPay/obj/Debug/net8.0/QuickPay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickPay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9f38ec0dde12777c4c82e1de9842e0c480268d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b8336721c11cb128c69cc11de4b1f044dafc2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickPay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickPay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
